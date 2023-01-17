bcde3f8b5f6e9ddd1a6af80104e5b888c43ce0aa
squidmin <morse.james.r@gmail.com>
2023-01-16 21:42:46 -0800

    first commit

---
+++ src/01_sliding_window/01_maximum_sum_subarray_of_size_k.py
@@ -0,0 +1,55 @@
+'''
+Given an array of positive numbers and a positive number `k`, find the maximum sum of
+  its contiguous subarrays of size `k`.
+
+Example 1:
+Input: [2, 1, 5, 1, 3, 2], k=3
+Output: 9
+Explanation: Subarray with maximum sum is [5, 1, 3].
+
+Example 2:
+Input: [2, 3, 4, 1, 5], k=2
+Output: 7
+Explanation: Subarray with maximum sum is [3, 4].
+'''
+
+'''
+Time complexity: O(N * K)
+  where N is the total number of elements in the given array.
+
+Space complexity: O(1)
+'''
+def max_sum_subarray_of_size_k_brute_force(k, arr):
+  max_sum = 0
+  window_sum = 0
+  for i in range(len(arr) - k + 1):
+    window_sum = 0
+    for j in range(i, i + k):
+      window_sum += arr[j]
+    max_sum = max(max_sum, window_sum)
+  return max_sum
+
+'''
+Time complexity: O(N)
+  where N is the total number of elements in the given array.
+
+Space complexity: O(1)
+'''
+def max_sum_subarray_of_size_k_optimized(k, arr):
+  max_sum, window_sum = 0, 0
+  window_start = 0
+  for window_end in range(len(arr)):
+    window_sum += arr[window_end]
+    if (window_end >= k - 1):
+      max_sum = max(max_sum, window_sum)
+      window_sum -= arr[window_start]
+      window_start += 1
+  return max_sum
+
+def main():
+  print("Maximum sum of a subarray of size K (brute force): " + str(max_sum_subarray_of_size_k_brute_force(3, [2, 1, 5, 1, 3, 2])))
+  print("Maximum sum of a subarray of size K (brute force): " + str(max_sum_subarray_of_size_k_brute_force(2, [2, 3, 4, 1, 5])))
+  print("Maximum sum of a subarray of size K (optimized): " + str(max_sum_subarray_of_size_k_optimized(3, [2, 1, 5, 1, 3, 2])))
+  print("Maximum sum of a subarray of size K (optimized): " + str(max_sum_subarray_of_size_k_optimized(2, [2, 3, 4, 1, 5])))
+
+main()
---
+++ src/02_two_pointers/01_pair_with_target_sum.py
@@ -0,0 +1,63 @@
+'''
+Problem statement
+
+Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
+
+
+Example 1:
+
+Input: [1, 2, 3, 4, 6], target=6
+Output: [1, 3]
+Explanation: The numbers at index 1 and 3 add up to 6: 2+4=6
+
+
+Example 2:
+
+Input: [2, 5, 9, 11], target=11
+Output: [0, 2]
+Explanation: The numbers at index 0 and 2 add up to 11: 2+9=11
+'''
+
+'''
+Time complexity: The time complexity of the above algorithm will be O(N), where N is the
+  total number of elements in the given array.
+
+Space complexity: O(1)
+'''
+def pair_with_targetsum_brute_force(arr, target_sum):
+  left, right = 0, len(arr) - 1
+  while(left < right):
+    current_sum = arr[left] + arr[right]
+    if current_sum == target_sum:
+      return [left, right]
+
+    if target_sum > current_sum:
+      left += 1  # We need a pair with a bigger sum.
+    else:
+      right -= 1  # We need a pair with a smaller sum.
+  return [-1, -1]
+
+'''
+Time complexity: O(N)
+  where N is the total number of elements in the given array.
+
+Space complexity: O(N), since in the worst case, the algorithm will push
+  N numbers into the HashTable.
+'''
+def pair_with_targetsum_optimized(arr, target_sum):
+  nums = {}  # To store numbers and their indices.
+  for i, num in enumerate(arr):    # i, num  ->  count, value
+    if target_sum - num in nums:
+      return [nums[target_sum - num], i]
+    else:
+      nums[arr[i]] = i
+  return [-1, -1]
+
+def main():
+  print(pair_with_targetsum_brute_force([1, 2, 3, 4, 6], 6))
+  print(pair_with_targetsum_brute_force([2, 5, 9, 11], 11))
+  print(pair_with_targetsum_optimized([1, 2, 3, 4, 6], 6))
+  print(pair_with_targetsum_optimized([2, 5, 9, 11], 11))
+
+main()
