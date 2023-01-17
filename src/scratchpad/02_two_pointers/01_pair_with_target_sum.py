3c4b494408bd99bd5bdf8b7a328f9e2932cad013
squidmin <morse.james.r@gmail.com>
2023-01-16 21:53:46 -0800

    Add practice files

---
+++ src/scratchpad/02_two_pointers/01_pair_with_target_sum.py
@@ -0,0 +1,13 @@
+def pair_with_targetsum_brute_force(arr, target_sum):
+  return None
+
+def pair_with_targetsum_optimized(arr, target_sum):
+  return None
+
+def main():
+  print(pair_with_targetsum_brute_force([1, 2, 3, 4, 6], 6))
+  print(pair_with_targetsum_brute_force([2, 5, 9, 11], 11))
+  print(pair_with_targetsum_optimized([1, 2, 3, 4, 6], 6))
+  print(pair_with_targetsum_optimized([2, 5, 9, 11], 11))
+
+main()
