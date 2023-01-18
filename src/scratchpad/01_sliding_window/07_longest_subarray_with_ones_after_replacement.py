2db8024ce560e88c16db1942387c6824ddc409c0
squidmin <morse.james.r@gmail.com>
2023-01-17 21:00:55 -0800

    Add: Longest subarray with ones after replacement

---
+++ src/scratchpad/01_sliding_window/07_longest_subarray_with_ones_after_replacement.py
@@ -0,0 +1,11 @@
+def length_of_longest_substring(arr, k):
+  # TODO: ...
+  return None
+
+
+def main():
+  print(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2))
+  print(length_of_longest_substring([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3))
+
+
+main()
