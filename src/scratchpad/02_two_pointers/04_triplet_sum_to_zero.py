dd1ef4887c921f31a33b863a4fdd160adc0749cc
squidmin <morse.james.r@gmail.com>
2023-01-18 23:22:56 -0800

    Add: Two pointers: Triplet sum to zero

---
+++ src/scratchpad/02_two_pointers/04_triplet_sum_to_zero.py
@@ -0,0 +1,11 @@
+def search_triplets(arr):
+    # TODO: ...
+    return None
+
+
+def main():
+    print(search_triplets([-3, 0, 1, 2, -1, 1, -2]))
+    print(search_triplets([-5, 2, -1, -2, 3]))
+
+
+main()
