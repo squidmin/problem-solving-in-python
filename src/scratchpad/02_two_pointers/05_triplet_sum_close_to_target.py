d134d3500a9a5c9e2cc349d7f93873559a5b8140
squidmin <morse.james.r@gmail.com>
2023-01-18 23:27:19 -0800

    Add: Two pointers: Triplet sum close to target

---
+++ src/scratchpad/02_two_pointers/05_triplet_sum_close_to_target.py
@@ -0,0 +1,15 @@
+import math
+
+
+def triplet_sum_close_to_target(arr, target_sum):
+    # TODO: ...
+    return None
+
+
+def main():
+    print(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
+    print(triplet_sum_close_to_target([-3, -1, 1, 2], 1))
+    print(triplet_sum_close_to_target([1, 0, 1, 1], 100))
+
+
+main()
